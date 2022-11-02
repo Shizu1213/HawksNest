819078e39fb05a5bf70d1fc0f600dda7ea31abc2
Kyle Rodrigues <rodrigueskyle44@gmail.com>
2022-11-02 16:23:27 -0400

    Added Background

--- player/src/App.js
+++ player/src/App.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-
-function Move(e){
-  alert(e.keyCode);
-}
+import map_entrance from './screens/map_entrance_v1.jpg'
 
 export default function App() {
-  const [coords, setCoords] = useState({x: 0, y: 0});
-  const [globalCoords, setGlobalCoords] = useState({x: 0, y: 0});
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [globalCoords, setGlobalCoords] = useState({ x: 0, y: 0 });
+  const [playerCoords, setPlayerCoords] = useState({ x: 680, y: 530 })
+  const [isMoving, setIsMoving] = useState(false)
 
   useEffect(() => {
     // get global mouse coordinates
@@ -25,18 +24,28 @@ export default function App() {
 
   }, []);
 
+  //Handles cords of mouse
   const handleMouseMove = event => {
     setCoords({
       x: event.clientX - event.target.offsetLeft,
       y: event.clientY - event.target.offsetTop,
     });
   };
+  
+  //Event Listeners
+  window.addEventListener('keydown', (event) => {
+    setIsMoving(true)
+    setPlayerCoords({x: playerCoords.x - 3, y: playerCoords.y})
+  })
+  window.addEventListener('keyup', (event) => {
+    setIsMoving(false);
+  })
+
 
   return (
-    <div className="App" onMouseMove={handleMouseMove}>
+    <div className="App" onMouseMove={handleMouseMove} style={{ backgroundImage: `url(${map_entrance})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }} >
       {coords.x} {coords.y}
-      <div className="Person" style={{left: coords.x, top: coords.y}}></div>
-
+      <div className="Person" style={{ left: playerCoords.x, top: playerCoords.y }}></div>
     </div>
   );
 }

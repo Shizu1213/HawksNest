723744e7a3195212f0a800944de1e416fb9feb88
Kyle Rodrigues <rodrigueskyle44@gmail.com>
2022-11-21 15:19:20 -0500

    sack

--- game/src/App.js
+++ game/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+import map_entrance_walls from './screens/map_entrance_v3.png';
 import map_entrance from './screens/map_entrance_v1.png';
 import star_bucks from './screens/star_bucks_v1.png';
 
@@ -7,7 +7,7 @@ import MapEntrance from './Components/MapEntrance';
 import './App.css';
 
 export default function App() {
-  const [backgroundImage, setBackGroundImage] = useState(`url(${map_entrance})`)
+  const [backgroundImage, setBackGroundImage] = useState(`url(${map_entrance_walls})`)
 
   return (
     <div className='Screen'>
